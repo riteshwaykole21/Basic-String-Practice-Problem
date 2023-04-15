c5445871072a51be25ef95654249acf007ba48a3
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-15 12:56:57 +0530

    [Ritesh]add : String Printing

---
+++ StringProblem/Print.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProblem
+{
+    public class Print
+    {
+        public void Input()
+        {
+            Console.WriteLine("Enter A One String");
+            string Name = Console.ReadLine();
+            Console.WriteLine($"{Name}");
+        }
+    }
+}
---
+++ StringProblem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : Input String And Print it");
+            Console.WriteLine("Enter A Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    Print print = new Print();
+                    print.Input();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
